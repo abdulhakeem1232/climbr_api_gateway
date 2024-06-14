5f1a47efc712a13c932d7b273a73ae4219542aad
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-06-14 15:47:32 +0530

    done last seen

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,6 +1,24 @@
 import { Server as SocketIOServer } from 'socket.io';
 import { Server as HTTPServer } from 'http';
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import amqp from 'amqplib';
+
+const access_key = process.env.ACCESS_KEY
+const secret_access_key = process.env.SECRET_ACCESS_KEY
+const bucket_region = process.env.BUCKET_REGION
+const bucket_name = process.env.BUCKET_NAME
+
+if (!access_key || !secret_access_key) {
+    throw new Error("AWS credentials are not provided.");
+}
+const s3: S3Client = new S3Client({
+    credentials: {
+        accessKeyId: access_key,
+        secretAccessKey: secret_access_key
+    },
+    region: process.env.BUCKET_REGION
+});
 
 const setupSocket = async (server: HTTPServer): Promise<SocketIOServer> => {
     const io = new SocketIOServer(server, {
@@ -30,12 +48,10 @@ const setupSocket = async (server: HTTPServer): Promise<SocketIOServer> => {
         };
         socket.on('getOnlineUsers', () => {
             console.log('00000======');
-
             socket.emit('onlineUsers', Array.from(onlineUsers.keys()));
         });
 
         socket.on('join', (userId) => {
-            console.log('join------------');
             onlineUsers.set(userId, socket.id);
             broadcastOnlineUsers();
             console.log(`User ${userId} is online`);
@@ -66,10 +82,19 @@ const setupSocket = async (server: HTTPServer): Promise<SocketIOServer> => {
             }
         });
 
-        socket.on('sendMessage', (data) => {
+        socket.on('sendMessage', async (data) => {
             console.log('Message received: ', data);
-            const { chatId, userId, message } = data;
-            const newMessage = { chatId, sender: userId, message, createdAt: new Date().toISOString() };
+            const { chatId, userId, message, filePath, fileType } = data;
+            let newMessage = { chatId, sender: userId, message, createdAt: new Date().toISOString(), filePath, fileType };
+            if (filePath.trim() != "") {
+                const getObjectParams = {
+                    Bucket: bucket_name,
+                    Key: filePath,
+                }
+                const getObjectCommand = new GetObjectCommand(getObjectParams);
+                const url = await getSignedUrl(s3, getObjectCommand, { expiresIn: 3600 });
+                newMessage.filePath = url
+            }
             channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(data)));
             io.emit('message', newMessage);
         });
